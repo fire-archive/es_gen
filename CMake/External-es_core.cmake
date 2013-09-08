d822bdc959b1862ae7d1f661045080542a9c983c
Ernest Lee <hellfire3k@gmail.com>
2013-09-07 18:40:45 -0700

    Update External-es_core.cmake
    Use my own es_core repository.

--- CMake/External-es_core.cmake
+++ CMake/External-es_core.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(es_core
-  GIT_REPOSITORY "https://github.com/TTimo/es_core.git"
+  GIT_REPOSITORY "https://github.com/fire/es_core.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/es_core"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/Run -DCMAKE_MODULE_PATH=${CMAKE_CURRENT_BINARY_DIR}/Run/CMake
 )
