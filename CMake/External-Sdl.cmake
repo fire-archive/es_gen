3daf0285484af9208f336a5180d32a2620f1d561
Ernest Lee <hellfire3k@gmail.com>
2013-09-07 19:32:17 -0700

    Update External-Sdl.cmake

--- CMake/External-Sdl.cmake
+++ CMake/External-Sdl.cmake
@@ -2,9 +2,6 @@ IF(UNIX)
 ExternalProject_Add(Sdl
   HG_REPOSITORY "https://hg.libsdl.org/SDL"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/Sdl"
-  CONFIGURE_COMMAND ./autogen.sh && ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/Run
-  BUILD_COMMAND make
-  INSTALL_COMMAND make install
-  BUILD_IN_SOURCE 1
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/Run/
 )
 ENDIF(UNIX)
