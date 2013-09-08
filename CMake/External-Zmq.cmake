26e3178c8be9f316c0f08cd42f59b4b8d17dba51
Ernest Lee <hellfire3k@gmail.com>
2013-09-07 21:30:21 -0700

    Change to the correct git address.

--- CMake/External-Zmq.cmake
+++ CMake/External-Zmq.cmake
@@ -1,6 +1,6 @@
 IF(WIN32)
 ExternalProject_Add(Zmq
-  GIT_REPOSITORY "git@github.com:zeromq/libzmq.git"
+  GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/libzmq"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/Run
 )
@@ -8,7 +8,7 @@ ENDIF(WIN32)
 
 IF(UNIX)
 ExternalProject_Add(Zmq
-  GIT_REPOSITORY "git@github.com:zeromq/libzmq.git"
+  GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/libzmq"
   CONFIGURE_COMMAND ./autogen.sh && ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/Run
   BUILD_COMMAND make
