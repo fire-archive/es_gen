c3a4025914b1dfa079d5edc983da58043e3fa629
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2013-09-09 09:23:24 -0700

    Don't use make file for libzmq.

--- CMake/External-Zmq.cmake
+++ CMake/External-Zmq.cmake
@@ -1,19 +1,6 @@
-IF(WIN32)
 ExternalProject_Add(Zmq
   GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
   PATCH_COMMAND git apply < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/zmq-Cmake.patch
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/libzmq"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/Run -DBUILD_TESTING=0
 )
-ENDIF(WIN32)
-
-IF(UNIX)
-ExternalProject_Add(Zmq
-  GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
-  PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/libzmq"
-  CONFIGURE_COMMAND ./autogen.sh && ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/Run
-  BUILD_COMMAND make
-  INSTALL_COMMAND make install
-  BUILD_IN_SOURCE 1
-)
-ENDIF(UNIX)
