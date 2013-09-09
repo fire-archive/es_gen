9bd2d0ab2a574c18fa6b29c192356374681ccdbf
K Ernest (iFire) Lee <fire@chibifire.com>
2013-09-09 02:04:35 -0700

    Update zmq with a patch that adds a BUILD_TESTING option and update czmq.

--- CMake/External-Czmq.cmake
+++ CMake/External-Czmq.cmake
@@ -1,5 +1,6 @@
 IF(UNIX)
 ExternalProject_Add(Czmq
+  DEPENDS Zmq
   GIT_REPOSITORY "https://github.com/fire/czmq.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/Czmq"
   CONFIGURE_COMMAND ./autogen.sh && ./configure --with-libzmq=${CMAKE_CURRENT_BINARY_DIR}/Run --prefix=${CMAKE_CURRENT_BINARY_DIR}/Run
@@ -11,7 +12,7 @@ ENDIF(UNIX)
 
 IF(WIN32)
 ExternalProject_Add(Czmq
-  DEPENDS Ogredeps
+  DEPENDS Zmq
   GIT_REPOSITORY "https://github.com/fire/czmq.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/Czmq"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/Run/ -DWITH_LIBZMQ=${CMAKE_CURRENT_BINARY_DIR}/Run/include
