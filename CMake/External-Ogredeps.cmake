6b53d602f2226f6764a86d13b7fbb0897fe1046a
K Ernest (iFire) Lee <fire@chibifire.com>
2013-09-08 23:48:51 -0700

    Make cmake try patching sdl.

--- CMake/External-Ogredeps.cmake
+++ CMake/External-Ogredeps.cmake
@@ -1,5 +1,6 @@
 ExternalProject_Add(Ogredeps
   HG_REPOSITORY "https://bitbucket.org/cabalistic/ogredeps" 
+  PATCH_COMMAND "git apply < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/SDL-Cmake.patch"
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/Tools/Ogredeps
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/Run/
 )
