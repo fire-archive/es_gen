12530c9059fac425d2ec2939e6a7478bdfc2fdd1
K Ernest (iFire) Lee <fire@chibifire.com>
2013-09-10 18:54:53 -0700

    Ignore whitespace in git patches.

--- CMake/External-Sdl.cmake
+++ CMake/External-Sdl.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(Sdl
   HG_REPOSITORY "https://hg.libsdl.org/SDL"
-  PATCH_COMMAND git apply < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/SDL-Cmake.patch
+  PATCH_COMMAND git apply --ignore-whitespace < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/SDL-Cmake.patch
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/Sdl"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/Run/
 )
