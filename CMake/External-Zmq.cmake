7b2d761c7a66bc3132ef9b8296d7976cb337a853
K Ernest (iFire) Lee <fire@chibifire.com>
2013-09-10 19:19:24 -0700

    Reset repository to original repo and apply patch.

--- CMake/External-Zmq.cmake
+++ CMake/External-Zmq.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(Zmq
   GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
-  PATCH_COMMAND git apply --ignore-whitespace < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/zmq-Cmake.patch
+  PATCH_COMMAND git reset --hard && git apply --ignore-whitespace < ${CMAKE_CURRENT_SOURCE_DIR}/CMake/zmq-Cmake.patch
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/libzmq"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/Run -DBUILD_TESTING=0
 )
