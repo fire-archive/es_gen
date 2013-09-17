9ee306322624d47fe2f07f2202da5cb15e9c2fae
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2013-09-17 15:12:06 -0700

    Update cmake files with correct depends and misc deletes.
    Update cmake so that the dependencies are correct for es_core, czmq and ogre.
    Also, delete the weird add dependencies commands.

--- CMake/External-Ogre.cmake
+++ CMake/External-Ogre.cmake
@@ -1,8 +1,7 @@
 ExternalProject_Add(Ogre
-  DEPENDS Ogredeps
+  DEPENDS Ogre Ogredeps
   HG_REPOSITORY "https://bitbucket.org/sinbad/ogre"
   HG_TAG "v1-8"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/Ogre"
   CMAKE_ARGS -DOGRE_DEPENDENCIES_DIR=${CMAKE_BINARY_DIR}/Run/ -DOGRE_BUILD_SAMPLES=0 -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/Run/
-
 )
--- CMake/External-es_core.cmake
+++ CMake/External-es_core.cmake
@@ -1,4 +1,5 @@
 ExternalProject_Add(es_core
+  DEPENDS es_core Ogre Czmq Sdl Zmq
   GIT_REPOSITORY "https://github.com/fire/es_core.git"
   PREFIX "${CMAKE_CURRENT_BINARY_DIR}/Tools/es_core"
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/Run -DCMAKE_MODULE_PATH=${CMAKE_CURRENT_BINARY_DIR}/Run/CMake
